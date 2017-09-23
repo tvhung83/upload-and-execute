003c4222e1a91870c59233bc29710c2011696146
Robert Nguyen <ng.hung83@gmail.com>
2017-09-23 12:45:49 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+file-upload-in-Node
+===================
+
+Download code and type 
+```node app.js``` or ```PORT=8080 node app.js```
+Visit ```localhost:8080``` to view the app.
+Upload file: ```$ curl -F 'file=@path/to/file' http://localhost:8080```
+
---
+++ app.js
@@ -0,0 +1,33 @@
+var express = require("express");
+var multer = require('multer');
+var app = express();
+var filename;
+var storage = multer.diskStorage({
+    destination: function(req, file, callback) {
+        callback(null, './uploads');
+    },
+    filename: function(req, file, callback) {
+        filename = file.fieldname + '-' + Date.now() + '.js';
+        callback(null, filename);
+    }
+});
+var upload = multer({
+    storage: storage
+}).single('file');
+
+app.post('/', function(req, res) {
+    upload(req, res, function(err) {
+        if (err) {
+            console.error(err);
+            return res.end("Error uploading file.");
+        }
+        res.end("File is uploaded");
+        const pong = require('./uploads/' + filename);
+        pong();
+        return true;
+    });
+});
+
+app.listen(process.env.port || process.env.PORT || 8080, function() {
+    console.log("Working on port %s", process.env.port || process.env.PORT || 8080);
+});
